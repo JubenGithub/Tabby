8e879b3cfe1cb31e4469b15b0368f7a9716307f4
jency267 <jency267@tamu.edu>
2018-04-06 08:30:06 -0500

    add onoff switch for prevent duplicate tabs

--- background.js
+++ background.js
@@ -2,6 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 var ctabs = [];
+var onoff = "Off";
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.trigger == "On")
+     	onoff = "On";
+	if (request.trigger == "Off")
+		onoff = "Off";
+  });
+
 chrome.runtime.onInstalled.addListener(function(e){
 	chrome.tabs.query({currentWindow: true}, function (tabs){
 		for(var i in tabs){
@@ -13,29 +22,29 @@ chrome.runtime.onInstalled.addListener(function(e){
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-	var url = changeInfo.url;
-	var activeId = tabId;
-	if (url){
-		chrome.tabs.query({currentWindow: true, active:false}, function (tabs){
-			for (var i = 0; i < tabs.length; i++){
-				if (tabs[i].url == url && tabs[i].id != activeId){
-					console.log(tabs[i].id);
-					chrome.tabs.move(activeId, {index:i+1});
-					chrome.tabs.remove(tabs[i].id);
-				} 
+	if(onoff == "On"){
+		var url = changeInfo.url;
+		var activeId = tabId;
+		if (url){
+			chrome.tabs.query({currentWindow: true, active:false}, function (tabs){
+				for (var i = 0; i < tabs.length; i++){
+					if (tabs[i].url == url && tabs[i].id != activeId){
+						console.log(tabs[i].id);
+						chrome.tabs.move(activeId, {index:i+1});
+						chrome.tabs.remove(tabs[i].id);
+					} 
+			}
+			});
 		}
-		});
 	}
 });
 chrome.tabs.onCreated.addListener(function(tab){
 	ctabs.push(tab.id);
-	console.log(ctabs);
 });
 chrome.tabs.onRemoved.addListener(function (tabId, removeInfo){
 	if(ctabs.includes(tabId)){
 		ctabs.splice(ctabs.indexOf(tabId), 1);
 	}
-	console.log(ctabs);
 })
 
 chrome.tabs.onActivated.addListener(function(activeInfo){
@@ -46,7 +55,6 @@ chrome.tabs.onActivated.addListener(function(activeInfo){
 	else{
 		ctabs.push(activeInfo.tabId);
 	}
-	console.log(ctabs);
 })
 
 
--- popup.js
+++ popup.js
@@ -1,11 +1,27 @@
 let sortname = document.getElementById('name');
 let sortfrequency = document.getElementById('frequency');
+let onoffswitch = document.getElementById('onoff');
+let background = chrome.extension.getBackgroundPage();
+if(background.onoff == "On"){
+	onoffswitch.checked = true;
+}
+else{
+	onoffswitch.checked = false;
+}
+onoffswitch.onclick = function(element){
+	if (onoffswitch.checked == true){
+		chrome.runtime.sendMessage({trigger: "On"});
+	}
+	else{
+		chrome.runtime.sendMessage({trigger: "Off"});
+	}
+}
 sortname.onclick = function(element){
 	chrome.tabs.query({currentWindow: true}, function (tabs){
 		console.log(tabs);
 		var sortarray = [];
 		for (var i = 0; i < tabs.length; i++ ){
-			sortarray.push([tabs[i].id, tabs[i].title]);
+			sortarray.push([tabs[i].id, tabs[i].url]);
 		}
 		sortarray.sort(function(a, b){return a[1].localeCompare(b[1])});
 		console.log(sortarray);
@@ -15,7 +31,6 @@ sortname.onclick = function(element){
 	});
 };
 sortfrequency.onclick = function(element){
-	let background = chrome.extension.getBackgroundPage();
 	let farray = background.ctabs;
 	console.log(farray);
 	for(var i in farray){
